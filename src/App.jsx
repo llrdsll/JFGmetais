3255ee34dc6de876747bf0bdc7b10e041fece44d
André2k <114704158+llrdsll@users.noreply.github.com>
2025-09-09 15:32:41 -0300

    subiu

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-orange-50">
+      <h1 className="text-4xl font-bold text-orange-600">Sua Loja Online 🚀</h1>
+    </div>
+  );
+}
